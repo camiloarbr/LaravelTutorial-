a9a54d589be0a69f1dc9748ac5eb5ea76720b817
Camilo <Carbelaezr@eafit.edu.co>
2025-08-11 20:53:18 -0500

    D. Product creation (simulation)

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,8 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index")
 Route::get('/about', function () {
 
     $data1 = "About us - Online Store";
-
     $data2 = "About us";
-
     $description = "This is an about page ...";
-
     $author = "Developed by: Your Name";
 
     return view('home.about')->with("title", $data1)
@@ -21,6 +18,8 @@ Route::get('/about', function () {
 
 })->name("home.about");
 
+// Orden correcto de rutas de productos
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
-
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
